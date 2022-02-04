96c7e50c56c1774f5ba1778741658509c79c56f9
Gabriel Lima <gabrielvitor180701@gmail.com>
2022-02-04 15:27:23 -0300

    Autenticando e finalizando primeira parte do projeto

--- escola/serializer.py
+++ escola/serializer.py
@@ -12,6 +12,34 @@ class CursoSerializer(serializers.ModelSerializer):
         fields = '__all__'
         
 class MatriculaSerializer(serializers.ModelSerializer):
+    aluno = serializers.ReadOnlyField(source='aluno.nome')  
+    curso = serializers.ReadOnlyField(source='curso.descricao')
+    periodo = serializers.SerializerMethodField()
     class Meta:
         model = Matricula
         exclude = []
+        
+    def get_periodo(self, obj):
+        return obj.get_periodo_display()
+        
+class ListaMatriculasAlunoSerializer(serializers.ModelSerializer):
+    # Deixando os campos com uma melhor visualização. 
+    # Ex: Ao inves do id 1 veremos Curso Python
+    curso = serializers.ReadOnlyField(source='curso.descricao')
+    # Ex: Ao inves de N veremos Noturno
+    periodo = serializers.SerializerMethodField()
+    class Meta:
+        model = Matricula
+        fields = ['curso', 'periodo']
+    
+    # Method do SerializerMethodField()
+    def get_periodo(self, obj):
+        return obj.get_periodo_display()
+    
+class ListaAlunosMatriculaSerializer(serializers.ModelSerializer):
+    # Deixando os campos com uma melhor visualização. 
+    # Ex: Ao inves do id 1 veremos Gabriel Lima
+    aluno = serializers.ReadOnlyField(source='aluno.nome')      
+    class Meta:
+        model = Matricula
+        fields = ['aluno']
--- escola/views.py
+++ escola/views.py
@@ -1,18 +1,49 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from .models import Aluno, Curso, Matricula
-from .serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer
+from .serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculaSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os alunos(as)"""
     queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     
 class CursosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os cursos"""
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     
 class MatriculaViewSet(viewsets.ModelViewSet):
     """Exibindo todas as matriculas"""
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+class ListaMatriculasAluno(generics.ListAPIView):
+    """Listando todas as matriculas de um aluno(a)"""
+    
+    # passando o queryset e o serializer_class como em todas outras views 
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasAlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+class ListaAlunosMatricula(generics.ListAPIView):
+    """Listando todos alunos(a) de um curso"""
+    
+    # passando o queryset e o serializer_class como em todas outras views 
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaAlunosMatriculaSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated ]
+    
