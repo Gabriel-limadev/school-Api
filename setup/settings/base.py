9b4fa9bbd4937e1957f695502bf6833560a14d19
Gabriel Lima <gabrielvitor180701@gmail.com>
2022-02-05 11:13:16 -0300

    Realizando Deploy

--- setup/settings/base.py
+++ setup/settings/base.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -12,10 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-7qm0b9+01t6pen3mz!=qzgo-h^_5w7)j)ak^dufy)fvz7vg3zl"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ["*"]
-
+DEBUG = True
 
 # Application definition
 
@@ -108,6 +105,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
